0162108cba8709f303d34a427bdef2dd02d12e10
dean.delponte <dean.delponte@9c36ab66-e58d-11dd-bd7b-b12b6590754f>
2009-01-19 16:58:20 +0000

    recaptcha plugin initial commit

---
+++ plugins/recaptcha-0.3.1/grails-app/taglib/RecaptchaTagLib.groovy
@@ -0,0 +1,49 @@
+
+class RecaptchaTagLib {
+	static namespace = "recaptcha"
+	RecaptchaService recaptchaService
+	private def attrNames = ["theme", "lang", "tabindex", "custom_theme_widget"]
+	
+	/**
+	 * Evaluates the content of the tag if ReCaptcha support is enabled. This value is set in 
+	 * grails-app/conf/RecaptchaConfig.groovy
+	 */
+	def ifEnabled = { attrs, body ->
+		if (recaptchaService.isEnabled()) {
+			out << body()
+		}
+	}
+	
+	/**
+	 * Create and display a ReCaptcha instance. Supports the following attributes:
+	 * <ul>
+	 * <li>theme - Can be one of 'red','white','blackglass','clean','custom'</li>
+	 * <li>lang  - Can be one of 'en','nl','fr','de','pt','ru','es','tr'</li>
+	 * <li>tabindex - Sets a tabindex for the ReCaptcha box</li>
+	 * <li>custom_theme_widget - Used when specifying a custom theme.</li>
+	 * </ul>
+	 * 
+	 * This tag can also be used in support of a custom theme. For more information about
+	 * custom themes, see: http://recaptcha.net/apidocs/captcha/client.html
+	 */
+	def recaptcha = { attrs ->
+		def props = new Properties()
+		attrNames.each {
+			if (attrs[it]) {
+				props.setProperty(it, attrs[it])
+			}
+		}
+		out << recaptchaService.createCaptcha(session, props)
+	}
+
+    /**
+     * Evaluates the content of the tag if ReCaptcha validation failed. This will allow
+     * developers to display errors for ReCaptcha themes that do not display error messages
+     * by default, like the 'clean' theme.
+     */
+    def ifFailed = { attrs, body ->
+        if (recaptchaService.validationFailed(session)) {
+            out << body()
+        }
+    }
+}
