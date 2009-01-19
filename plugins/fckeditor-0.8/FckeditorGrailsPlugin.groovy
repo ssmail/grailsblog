bf2577ba458535490eb3130cd600ccf5f104f00a
dean.delponte <dean.delponte@9c36ab66-e58d-11dd-bd7b-b12b6590754f>
2009-01-19 16:17:48 +0000

    fckeditor checkin complete

---
+++ plugins/fckeditor-0.8/FckeditorGrailsPlugin.groovy
@@ -0,0 +1,28 @@
+class FckeditorGrailsPlugin {
+    def version = '0.8'
+    def dependsOn = [:]
+	
+    def author = "Stefano Gualdi"
+    def authorEmail = "stefano.gualdi@gmail.com"
+    def title = "Fckeditor"
+    def description = 'Native Grails plugin for using FCKeditor rich editing features in a Grails application'
+    def documentation = "http://grails.org/FCKeditor+plugin"
+	
+    def doWithSpring = {
+    }
+   
+    def doWithApplicationContext = { applicationContext ->
+    }
+
+    def doWithWebDescriptor = { xml ->
+    }
+	                                      
+    def doWithDynamicMethods = { ctx ->
+    }
+	
+    def onChange = { event ->
+    }
+                                                                                  
+    def onApplicationChange = { event ->
+    }
+}
