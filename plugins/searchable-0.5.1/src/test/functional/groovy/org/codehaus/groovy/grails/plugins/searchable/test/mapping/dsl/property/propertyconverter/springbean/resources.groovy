fc8ee5168379fa82dca03ec2432f8f90521c421c
dean.delponte <dean.delponte@9c36ab66-e58d-11dd-bd7b-b12b6590754f>
2009-01-20 03:59:18 +0000

    Commiting searchable in small chunks... goole's svn can't seem to handle large amounts of data

---
+++ plugins/searchable-0.5.1/src/test/functional/groovy/org/codehaus/groovy/grails/plugins/searchable/test/mapping/dsl/property/propertyconverter/springbean/resources.groovy
@@ -0,0 +1,6 @@
+package org.codehaus.groovy.grails.plugins.searchable.test.mapping.dsl.property.propertyconverter.springbean
+
+beans = {
+    // implements Conveter, so automatically registered with its name
+    myValueConverter(ValueConverter)
+}
